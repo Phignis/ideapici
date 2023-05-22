25fa9d07ce5ccafbae7a0fb2e6aff3566349f9dd
Phignis <baptiste63.foucras.pro@gmail.com>
2023-05-22 21:00:46 +0200

    add comfortaa and poppins fonts in assets, css color and font variables have been defined, and creation of unknown page (404), routing to 404 enabled

--- src/ideapici/src/app/app-routing.module.ts
+++ src/ideapici/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UnknownPageComponent } from './unknown-page/unknown-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "**", component: UnknownPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/ideapici/src/app/app.module.ts
+++ src/ideapici/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnknownPageComponent } from './unknown-page/unknown-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnknownPageComponent
   ],
   imports: [
     BrowserModule,

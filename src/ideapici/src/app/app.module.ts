32b8be6dc655813805348e1ea9d0fb8e5f0dec83
Thibault <t.lantzener@gmail.com>
2023-05-24 21:52:58 +0200

    change share-button and sharing-link-button components to directory components

--- src/ideapici/src/app/app.module.ts
+++ src/ideapici/src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { SetupChoiceComponent } from './pages/setup-choice/setup-choice.component';
-import { ShareButtonComponent } from './share-button/share-button.component';
-import { SharingLinkButtonComponent } from './sharing-link-button/sharing-link-button.component';
+import { ShareButtonComponent } from './components/share-button/share-button.component';
+import { SharingLinkButtonComponent } from './components/sharing-link-button/sharing-link-button.component';
 
 @NgModule({
   declarations: [

c6854ec2c0bb3c8c4276c4122b23953501b8fd02
Phignis <baptiste63.foucras.pro@gmail.com>
2023-05-25 16:41:12 +0200

    resolve implementation of remove interface, which was forgotten at last commit

--- src/ideapici/src/app/components/icons/people-profile-icon/people-profile-icon.component.ts
+++ src/ideapici/src/app/components/icons/people-profile-icon/people-profile-icon.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { SVGComponent } from 'src/app/interfaces/svgcomponent';
 
 @Component({
   selector: 'app-people-profile-icon',
   templateUrl: './people-profile-icon.component.html',
   styleUrls: ['./people-profile-icon.component.css']
 })
-export class PeopleProfileIconComponent implements SVGComponent {
+export class PeopleProfileIconComponent {
   @Input()
   public fillColor : string = '#78D6C6'
 }

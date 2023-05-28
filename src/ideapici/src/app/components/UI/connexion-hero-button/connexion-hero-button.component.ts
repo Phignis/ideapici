de72d8cea31d5f0fefbb08579c06175013429275
Phignis <baptiste63.foucras.pro@gmail.com>
2023-05-29 00:50:57 +0200

    add style for text or password input, add events to style the connexion part

--- src/ideapici/src/app/components/UI/connexion-hero-button/connexion-hero-button.component.ts
+++ src/ideapici/src/app/components/UI/connexion-hero-button/connexion-hero-button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, ElementRef, HostListener, QueryList, Renderer2, ViewChildren } from '@angular/core';
+import { SVGComponentComponent } from '../../icons/svgcomponent/svgcomponent.component';
 
 @Component({
   selector: 'app-connexion-hero-button',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class ConnexionHeroButtonComponent {
 
+  protected styleRoot = window.getComputedStyle(document.body);
+
+  @ContentChild(SVGComponentComponent)
+  private svgDisplayed! : SVGComponentComponent;
+
+  protected elementsVisible : boolean = false;
+
+  constructor() {}
+
+  @HostListener("mouseenter")
+  private onMouseEnter() {
+    this.svgDisplayed.fillColor = this.styleRoot.getPropertyValue('--interactible-color');
+  }
+
+  @HostListener("mouseleave")
+  private onMouseLeaving() {
+    this.svgDisplayed.fillColor = this.styleRoot.getPropertyValue('--primary-color');
+  }
+
+  protected revealContent() {
+    this.elementsVisible = !this.elementsVisible;
+    this.svgDisplayed.height = this.elementsVisible ? "24px" : "48px";
+    this.svgDisplayed.width = this.elementsVisible ? "24px" : "48px";
+  }
 }

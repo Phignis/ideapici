a58c4fc9639b60a11261a3b7b157ba2de082e5fc
Phignis <baptiste63.foucras.pro@gmail.com>
2023-05-29 09:18:52 +0200

    add content part of landing page expendable using ngClass

--- src/ideapici/src/app/pages/landing-page/landing-page.component.ts
+++ src/ideapici/src/app/pages/landing-page/landing-page.component.ts
@@ -6,8 +6,12 @@ import { Component, Input, ViewChild } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
+
+  protected isContentExpended : boolean = false;
+
   protected expendContent() {
     // TODO: create landing page content and expend the section
     console.log("hey");
+    this.isContentExpended = !this.isContentExpended;
   }
 }
